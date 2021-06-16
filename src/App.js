945241ce904170fb4e09459ff9cbbbcfc7a8937e
Ryan Rigley <ryanrigley@gmail.com>
2021-06-15 23:55:25 -0700

    removing all changes from last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+//import React libraries
 import React, { useState, useEffect } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReactGa from "react-ga";
-import { createBrowserHistory } from "history";
 
+//import CSS
 import "./App.css";
 
 //import components
 import AboutMePage from "./components/about-me-page/AboutMePage";
-
 import MyWorkPage from "./components/my-work-page/MyWorkPage";
 
 function App() {
   //Google analytics
-  var history = createBrowserHistory();
-
   useEffect(() => {
     ReactGa.initialize("G-3NF43NTEZV");
     //monitor page views
     ReactGa.pageview("/");
     ReactGa.pageview("/my-work");
   }, []);
+
   //states
   const [currentTags, setCurrentTags] = useState([]);
   const [videoPanelObject, setVideoPanelObject] = useState({
@@ -30,7 +29,7 @@ function App() {
 
   return (
     <>
-      <Router history={history}>
+      <Router>
         <Switch>
           <Route path="/" exact render={(props) => <AboutMePage {...props} />} />
 
