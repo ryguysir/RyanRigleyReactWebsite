59ff1668150218a00d46e7969e08bec00d066d65
ryguysir <ryanrigley@gmail.com>
2021-07-02 15:40:00 -0700

    working on meta tags

--- src/components/about-me-page/AboutMePage.js
+++ src/components/about-me-page/AboutMePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import ReactGa from "react-ga";
+import MetaTags from "react-meta-tags";
 
 import "./app.css";
 import "../../App.css";
@@ -24,48 +25,55 @@ const AboutMePage = ({ showAboutMe, setShowAboutMe }) => {
     document.getElementsByClassName("about-me")[0].scroll(0, 0);
   };
   return (
-    <div className="about-me">
-      {/* header */}
-      <AboutMeSection
-        myNameIs={true}
-        vidSrc={""}
-        body="Video Editor | Motion Graphics Artist | 3D Generalist | Web Developer"
-        vidOrImage={false}
-        buttons={true}
-        showAboutMe={showAboutMe}
-        setShowAboutMe={setShowAboutMe}
-        scrollToContactMe={scrollToContactMe}
-        centerOrFooter={false}
-      />
+    <>
+      <MetaTags>
+        <title>Ryan Rigley</title>
+        <meta id="meta-description" name="description" content="learn about Ryan Rigley" />
+        <meta id="og-title" property="og:title" content="Ryan Rigley" />
+      </MetaTags>
+      <div className="about-me">
+        {/* header */}
+        <AboutMeSection
+          myNameIs={true}
+          vidSrc={""}
+          body="Video Editor | Motion Graphics Artist | 3D Generalist | Web Developer"
+          vidOrImage={false}
+          buttons={true}
+          showAboutMe={showAboutMe}
+          setShowAboutMe={setShowAboutMe}
+          scrollToContactMe={scrollToContactMe}
+          centerOrFooter={false}
+        />
 
-      {/* video editor */}
-      <AboutMeSection
-        myNameIs={false}
-        vidSrc={"./videos/Video Editor Reel.mp4"}
-        body="Over a decade of experience editing videos of all types, from blockbuster trailers to corporate event pieces."
-        vidOrImage={true}
-        buttons={false}
-        showAboutMe={showAboutMe}
-        setShowAboutMe={setShowAboutMe}
-        scrollToContactMe={scrollToContactMe}
-        centerOrFooter={true}
-      />
+        {/* video editor */}
+        <AboutMeSection
+          myNameIs={false}
+          vidSrc={"./videos/Video Editor Reel.mp4"}
+          body="Over a decade of experience editing videos of all types, from blockbuster trailers to corporate event pieces."
+          vidOrImage={true}
+          buttons={false}
+          showAboutMe={showAboutMe}
+          setShowAboutMe={setShowAboutMe}
+          scrollToContactMe={scrollToContactMe}
+          centerOrFooter={true}
+        />
 
-      {/* motion graphics */}
-      <AboutMeSection
-        myNameIs={false}
-        vidSrc={"./videos/Motion Graphics Reel.mp4"}
-        body="Animated motion graphics, title design, 3D design, motion tracking, and compositing are all examples of skills that I can utilize in taking any video to the next level."
-        vidOrImage={true}
-        buttons={false}
-        showAboutMe={showAboutMe}
-        setShowAboutMe={setShowAboutMe}
-        scrollToContactMe={scrollToContactMe}
-        centerOrFooter={true}
-      />
+        {/* motion graphics */}
+        <AboutMeSection
+          myNameIs={false}
+          vidSrc={"./videos/Motion Graphics Reel.mp4"}
+          body="Animated motion graphics, title design, 3D design, motion tracking, and compositing are all examples of skills that I can utilize in taking any video to the next level."
+          vidOrImage={true}
+          buttons={false}
+          showAboutMe={showAboutMe}
+          setShowAboutMe={setShowAboutMe}
+          scrollToContactMe={scrollToContactMe}
+          centerOrFooter={true}
+        />
 
-      <ContactMe scrollToTop={scrollToTop} />
-    </div>
+        <ContactMe scrollToTop={scrollToTop} />
+      </div>
+    </>
   );
 };
 
--- src/components/my-work-page/MyWorkPage.js
+++ src/components/my-work-page/MyWorkPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-
+import MetaTags from "react-meta-tags";
 import { Link } from "react-router-dom";
 import "./app.css";
 import "../../App.css";
@@ -63,64 +63,68 @@ const MyWorkPage = ({ currentTags, setCurrentTags, videoPanelObject, setVideoPan
   };
 
   return (
-    <div className="my-work">
-      {/*__________________________about me button______________________________*/}
-      <Link to="/">
-        <div
-          className="my-work-bttn"
-          onClick={() => {
-            setCurrentTags([]);
-          }}
-        >
+    <>
+      <MetaTags>
+        <title>Projects by Ryan Rigley</title>
+        <meta id="meta-description" name="description" content="using tags, look through the projects created by Ryan Rigley" />
+        <meta id="og-title" property="og:title" content="Projects by Ryan Rigley" />
+      </MetaTags>
+      <div className="my-work">
+        {/*__________________________about me button______________________________*/}
+        <Link to="/">
+          <div
+            className="my-work-bttn"
+            onClick={() => {
+              setCurrentTags([]);
+            }}
+          >
+            <Fade top cascade>
+              <i className="fas fa-chevron-left" style={{ marginRight: 20 + "px", marginLeft: -5 + "px" }}></i>
+              About Me
+            </Fade>
+          </div>
+        </Link>
+
+        {/*_______________________________tags___________________________________ */}
+        <div id="tag-container">
+          {tagsText.map((item) => (
+            <Tag text={item} key={item} currentTags={currentTags} setCurrentTags={setCurrentTags} />
+          ))}
+          <SearchBttn showSearch={showSearch} />
+          <SearchForm currentTags={currentTags} setCurrentTags={setCurrentTags} />
+        </div>
+
+        {/*_____________________________disclaimer________________________________*/}
+        <div className={`disclaimer-container ${currentTags.length < 1 ? "" : "hidden"}`}>
           <Fade top cascade>
-            <i
-              className="fas fa-chevron-left"
-              style={{ marginRight: 20 + "px", marginLeft: -5 + "px" }}
-            ></i>
-            About Me
+            <i className="fas fa-chevron-up"></i>
+            <div className="disclaimer">Select tags from above and logos from below</div>
+            <i className="fas fa-chevron-down"></i>
           </Fade>
         </div>
-      </Link>
 
-      {/*_______________________________tags___________________________________ */}
-      <div id="tag-container">
-        {tagsText.map((item) => (
-          <Tag text={item} key={item} currentTags={currentTags} setCurrentTags={setCurrentTags} />
-        ))}
-        <SearchBttn showSearch={showSearch} />
-        <SearchForm currentTags={currentTags} setCurrentTags={setCurrentTags} />
+        {/*_____________________________projects____________________________________*/}
+        <div className={`project-holder ${currentTags.length < 1 ? "hidden" : ""}`}>
+          {projectTagSort().map((project) => (
+            <Link key={Math.random() * 100} to={`/my-work/${project.projectTitle}`}>
+              <Project
+                key={project.projectTitle}
+                award={project.award}
+                projectTitle={project.projectTitle}
+                projectText={project.projectText}
+                imgSrc={project.imgSrc}
+                projectVideos={project.projectVideos}
+                videoPanelObject={videoPanelObject}
+                setVideoPanelObject={setVideoPanelObject}
+              />
+            </Link>
+          ))}
+        </div>
+
+        {/*___________________________company logos_____________________________*/}
+        <CompanyLogos currentTags={currentTags} setCurrentTags={setCurrentTags} />
       </div>
-
-      {/*_____________________________disclaimer________________________________*/}
-      <div className={`disclaimer-container ${currentTags.length < 1 ? "" : "hidden"}`}>
-        <Fade top cascade>
-          <i className="fas fa-chevron-up"></i>
-          <div className="disclaimer">Select tags from above and logos from below</div>
-          <i className="fas fa-chevron-down"></i>
-        </Fade>
-      </div>
-
-      {/*_____________________________projects____________________________________*/}
-      <div className={`project-holder ${currentTags.length < 1 ? "hidden" : ""}`}>
-        {projectTagSort().map((project) => (
-          <Link key={Math.random() * 100} to={`/my-work/${project.projectTitle}`}>
-            <Project
-              key={project.projectTitle}
-              award={project.award}
-              projectTitle={project.projectTitle}
-              projectText={project.projectText}
-              imgSrc={project.imgSrc}
-              projectVideos={project.projectVideos}
-              videoPanelObject={videoPanelObject}
-              setVideoPanelObject={setVideoPanelObject}
-            />
-          </Link>
-        ))}
-      </div>
-
-      {/*___________________________company logos_____________________________*/}
-      <CompanyLogos currentTags={currentTags} setCurrentTags={setCurrentTags} />
-    </div>
+    </>
   );
 };
 
